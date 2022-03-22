538a4c9557afa144a859fc620a6d466c5402f52c
NicholassJP <nicholass.j.purnama@sat.co.id>
2022-03-22 17:05:44 +0700

    update

--- js/morris-data-sales.js
+++ js/morris-data-sales.js
@@ -10,164 +10,164 @@ Morris.Area({
     data: [
         {
         y: '2021-01',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+        lowinven: 40000,
+        properinven: 20000,
+        sales: 24000,
+        nosales: 30000
     }, {
         y: '2021-02',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+        lowinven: 30000,
+        properinven: 35000,
+        sales: 10000,
+        nosales: 25000
     },{
         y: '2021-03',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+        lowinven: 25000,
+        properinven: 28000,
+        sales: 15000,
+        nosales: 55000
     }, {
         y: '2021-04',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+        lowinven: 50000,
+        properinven: 50000,
+        sales: 29000,
+        nosales: 25000
     }, {
         y: '2021-05',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 45000,
+        properinven: 24000,
+        sales: 35000,
+        nosales: 15000
     }, {
         y: '2021-06',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 25000,
+        properinven: 25000,
+        sales: 24000,
+        nosales: 25000
     }, {
         y: '2021-07',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 35000,
+        properinven: 50000,
+        sales: 55000,
+        nosales: 30000
     }, {
         y: '2021-08',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 15000,
+        properinven: 18000,
+        sales: 20000,
+        nosales: 28000
     },
     {
         y: '2021-09',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 25000,
+        properinven: 28000,
+        sales: 32500,
+        nosales: 40000
     },
     {
         y: '2021-10',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 28000,
+        properinven: 55000,
+        sales: 20000,
+        nosales: 17000
     },
     {
         y: '2021-11',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 35000,
+        properinven: 25000,
+        sales: 30000,
+        nosales: 15000
     },
     {
         y: '2021-12',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 44000,
+        properinven: 15000,
+        sales: 50000,
+        nosales: 30000
     },
     {
         y: '2022-01',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 25000,
+        properinven: 18000,
+        sales: 25000,
+        nosales: 40000
     },
     {
         y: '2022-02',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 33000,
+        properinven: 25000,
+        sales: 14000,
+        nosales: 45000
     },
     {
         y: '2022-03',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 25000,
+        properinven: 40000,
+        sales: 12000,
+        nosales: 55000
     },
     {
         y: '2022-04',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 21000,
+        properinven: 45000,
+        sales: 19000,
+        nosales: 30000
     },
     {
         y: '2022-05',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 50000,
+        properinven: 25000,
+        sales: 18000,
+        nosales: 25000
     },
     {
         y: '2022-06',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 35000,
+        properinven: 40000,
+        sales: 27500,
+        nosales: 15000
     },
     {
         y: '2022-07',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 25000,
+        properinven: 30000,
+        sales: 50000,
+        nosales: 25000
     },
     {
         y: '2022-08',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 15000,
+        properinven: 50000,
+        sales: 15000,
+        nosales: 35000
     },
     {
         y: '2022-09',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 22000,
+        properinven: 45000,
+        sales: 25000,
+        nosales: 27500
     },
     {
         y: '2022-10',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 33000,
+        properinven: 28000,
+        sales: 30000,
+        nosales: 50000
     },
     {
         y: '2022-11',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 45000,
+        properinven: 25000,
+        sales: 40000,
+        nosales: 60000
     },
      {
         y: '2022-12',
-        lowinven: 250,
-        properinven: 150,
-        sales: 200,
-        nosales: 500
+         lowinven: 10000,
+        properinven: 55000,
+        sales: 20000,
+        nosales: 25000
     }],
     xkey: 'y',
     ykeys: ['lowinven', 'properinven', 'sales', 'nosales'],
